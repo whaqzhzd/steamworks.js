8982a6cd211855e5ff5e4406346ac2449fc20388
wanghanbin <544430497@qq.com>
2023-10-28 21:14:41 +0800

    format

---
+++ src/encrypt.rs
@@ -0,0 +1,3 @@
+use napi::{Env, JsObject};
+
+pub fn get_module_object(env: Env, main_module: JsObject, this_exports: JsObject) {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 use napi::bindgen_prelude::Error;
+use napi::{CallContext, JsNumber, JsObject};
+use napi::{Env, JsString};
 use napi_derive::napi;
+use napi_derive::{js_function, module_exports};
 use steamworks::AppId;
 use steamworks::Client;
-
 pub mod client;
 
 extern crate rmp_serde as rmps;
@@ -11,6 +13,8 @@ extern crate serde_derive;
 
 #[macro_use]
 extern crate lazy_static;
+
+pub mod encrypt;
 
 #[napi]
 pub fn init(app_id: u32) -> Result<(), Error> {
@@ -57,6 +61,40 @@ pub fn restart_app_if_necessary(app_id: u32) -> bool {
 #[napi]
 pub fn run_callbacks() {
     client::get_single().run_callbacks();
+}
+
+#[module_exports]
+fn mod_init(mut exports: JsObject, env: Env) -> napi::Result<()> {
+    let global = env.get_global()?;
+
+    let main_modeul = {
+        if global.get_named_property::<JsObject>("module").is_ok() {
+            // electron renderer
+            global.get_named_property::<JsObject>("module").unwrap()
+        } else {
+            let process: JsObject = global.get_named_property("process").unwrap();
+            let argv: JsObject = process.get_named_property("argv").unwrap();
+            let leng = argv.get_array_length().unwrap();
+
+            for x in 0..leng {
+                let arg: JsString = argv.get_element::<JsString>(x).unwrap();
+                if arg.into_utf8()?.as_str()?.contains("--inspect")
+                    || arg
+                        .into_utf8()?
+                        .as_str()?
+                        .contains("--remote-debugging-port")
+                {
+                    return Ok(());
+                }
+            }
+
+            process
+                .get_named_property::<JsObject>("mainModule")
+                .unwrap()
+        }
+    };
+
+    Ok(())
 }
 
 pub mod api;
